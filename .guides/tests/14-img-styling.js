716dd2b055f44f71402c6bb3179543ae5b7b0391
mathvdh <m@dcy.io>
2015-07-31 10:03:56 +0000

    cheerio not ok for live css, jsdom not ok for live css, phantomjs ok for live css

--- .guides/tests/14-img-styling.js
+++ .guides/tests/14-img-styling.js
@@ -1,62 +1,83 @@
 var fs = require('fs');
-var jsdom = require('jsdom');
+var phantom = require('phantom');
 
-
-var errors = [];
 var box_domain = process.env.CODIO_BOX_DOMAIN;
 var fullurl = "http://"+ box_domain + "/14-img-styling/image.html";
+var errors = [];
 
-jsdom.env(
-  fullurl,
-  ["http://code.jquery.com/jquery.js"],
-  {
-    features: {
-      FetchExternalResources : ["css"]
-    }
-  },
-  function (err, window) {
-     if (err) {
-       errors.push("Error loading image.html");
-     }
-    
-    var img = window.$("img")[0];
-    
-    if (img.style.borderBottomColor != "DarkGreen") {
-      errors.push("Bottom border color is not DarkGreen")
-    }
-    
-    if (img.style.bordeLeftColor != "DarkGreen") {
-      errors.push("Left border color is not DarkGreen")
-    }
-    
-    if (img.style.borderRightColor != "LightGreen") {
-      errors.push("Right border color is not LightGreen")
-    }
-    
-    if (img.style.borderTopColor != "LightGreen") {
-      errors.push("Top border color is not LightGreen")
-    }
-    
-    if (img.style.borderStyle != "solid") {
-      errors.push("Border style is not solid")
-    }
-    
-    if (img.style.borderWidth != "3px") {
-      errors.push("Border width is not 3px : "+img.style.borderWidth )
-    }
-    
-    if( errors.length <= 0 ) {
-        process.stdout.write('Well done!!!')
-        process.exit(0);
-    }
-    else {
-        process.stdout.write(errors.join("\n"))
-        process.exit(1);
-    }
+//console.log(fullurl);
 
-    
-  }
-);
+phantom.create(function (ph) {
+  ph.createPage(function (page) {
+    page.open(fullurl, function (status) {
+      //console.log("opened ? ", status);
+      page.evaluate(function () { 
+        
+        var errors =  [];
+        
+        var img = document.querySelector("img");
+        var style = getComputedStyle(img,null);
+        
+        //errors.push(style.borderBottomColor);
+        
+        var darkgreen = "rgb(0, 100, 0)";
+        var ligthgreen = "rgb(144, 238, 144)";
+
+        if (style.borderBottomColor != darkgreen) {
+          errors.push("Bottom border color is not DarkGreen")
+        }
+
+        if (style.borderLeftColor != darkgreen) {
+          errors.push("Left border color is not DarkGreen")
+        }
+
+        if (style.borderRightColor != ligthgreen) {
+          errors.push("Right border color is not LightGreen")
+        }
+
+        if (style.borderTopColor != ligthgreen) {
+          errors.push("Top border color is not LightGreen")
+        }
+
+        if (style.borderTopStyle != "solid" || style.borderBottomStyle != "solid" || style.borderRightStyle != "solid" || style.borderLeftStyle != "solid") {
+          errors.push("Border style is not solid")
+        }
+
+        if (style.borderTopWidth != "3px" || style.borderBottomWidth != "3px" || style.borderLeftWidth != "3px" || style.borderRightWidth != "3px") {
+          errors.push("Border width is not 3px : "+img.style.borderWidth )
+        }
+
+        return errors;
+        
+      
+      }, function (retErrors) {
+        
+        if (!retErrors) {
+          process.stdout.write("There was a problem running the test");
+          ph.exit();
+          process.exit(1);
+        } else {
+          if(retErrors.length <= 0 ) {
+            process.stdout.write('Well done!!!')
+            ph.exit();
+            process.exit(0);
+          }
+          else {
+            process.stdout.write(retErrors.join("\n"))
+            ph.exit();
+            process.exit(1);
+          }
+        }
+        
+      });
+    });
+  });
+});
 
 
 
+
+
+
+
+  
